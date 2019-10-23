a5ceb6ed958f16fe5a6d9b9c9269544779b9830e
czyanthony <56176547+czyanthony@users.noreply.github.com>
2019-10-23 15:06:48 -0700

    IMDONEv2

--- StudentRecordTester.java
+++ StudentRecordTester.java
@@ -19,6 +19,8 @@ public class StudentRecordTester
         students[2] =  new StudentRecord(c);
         
         System.out.println(students[0].finalAverage());
+        System.out.println(students[1].finalAverage());
+        System.out.println(students[2].finalAverage());
         
         
         
